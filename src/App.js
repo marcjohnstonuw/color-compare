e1a2e0fe7368e3bb8c15f47e312913148c765823
Marc Johnston <mjohnston@konradgroup.com>
2019-01-09 21:37:59 -0500

    initial version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import ColorContainer from './ColorContainer';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      colors: [
+        {h: 120, s: 50, l: 50},
+        {h: 120, s: 50, l: 50},
+        {h: 120, s: 50, l: 50},
+        {h: 120, s: 50, l: 50},
+        {h: 120, s: 50, l: 50},
+        {h: 120, s: 50, l: 50},
+        {h: 120, s: 50, l: 50},
+        {h: 120, s: 50, l: 50},
+        {h: 120, s: 50, l: 50},
+      ],
+    }
+  }
+  updateColor(e, index) {
+    const { colors } = this.state;
+    console.log(e.target.name);
+    this.setState({
+      colors: [
+        ...colors.slice(0, index),
+        {
+          ...colors[index],
+          [e.target.name]: e.target.value
+        },
+        ...colors.slice(index + 1),
+      ]
+      })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <main>
+          <div className="colors">
+          {this.state.colors.map((c, index) => 
+            <ColorContainer
+              color={this.state.colors[index]}
+              onUpdate={(e) => this.updateColor(e, index)}
+            ></ColorContainer>
+          )}
+          </div>
+        </main>
       </div>
     );
   }
---
+++ src/ColorContainer.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './ColorContainer.css';
+
+export default function render(props) {
+    const {h, s, l} = props.color;
+    console.log(props);
+    console.log(`hsl(${h}, ${s}%, ${l}%)`);
+    return (
+        <div className="color-container">
+            <div 
+                className="color-view"
+                style={{ backgroundColor: `hsl(${h}, ${s}%, ${l}%)`, border: '1px solid black'}}
+            />
+            <div className="controls">
+                <div>
+                    Hue: 
+                    <input value={h} onChange={props.onUpdate} />
+                </div>
+                <input 
+                    name="h"
+                    type="range"
+                    min="0"
+                    max="360"
+                    value={h}
+                    onChange={props.onUpdate}
+                />
+                
+                <div>
+                    Sat: 
+                    <input value={s} onChange={props.onUpdate} />
+                </div>
+                <input 
+                    name="s"
+                    type="range"
+                    min="0"
+                    max="100"
+                    value={s}
+                    onChange={props.onUpdate}
+                />
+                
+                <div>
+                    Light: 
+                    <input value={l} onChange={props.onUpdate} />
+                </div>
+                <input 
+                    name="l"
+                    type="range"
+                    min="0"
+                    max="100"
+                    value={l}
+                    onChange={props.onUpdate}
+                />
+            </div>
+        </div>
+    );
+}
