469be2dcc2c802c9ca8b00c4857ef870cbcdc330
Marc Johnston <mjohnston@konradgroup.com>
2019-01-23 22:55:47 -0500

    Updates

---
+++ precache-manifest.d83dd24dad5689411ca0635c25e78a34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/color-compare/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "801ba1267f4be3c985cc",
+    "url": "/color-compare/static/js/runtime~main.801ba126.js"
+  },
+  {
+    "revision": "a297858466b41f8594a7",
+    "url": "/color-compare/static/js/main.a2978584.chunk.js"
+  },
+  {
+    "revision": "5089773eaabbbd9822f9",
+    "url": "/color-compare/static/js/1.5089773e.chunk.js"
+  },
+  {
+    "revision": "a297858466b41f8594a7",
+    "url": "/color-compare/static/css/main.1ee18eac.chunk.css"
+  },
+  {
+    "revision": "bf61e04e345b25aff4ef7de02d65adaf",
+    "url": "/color-compare/index.html"
+  }
+];
